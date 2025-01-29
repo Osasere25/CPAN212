bbb326aa2e6783bb5595329bb6127ba67145cc21
Osasere <jasmineobazo25@gmail.com>
2025-01-29 02:24:30 -0500

    create lab 2 folder

---
+++ labs/lab02/routes.js
@@ -0,0 +1,55 @@
+const express = require ('express');
+const router = express.Router();
+
+router.get('/name', (req, res) => {
+    res.send('Osasere Obazogobn');
+});
+
+router.get('/greeting', (req, res) => {
+    res.send('Osasere Obazogbon - Student Number: N01651999');
+});
+
+router.get('/add', (req, res) => {
+    const x = parseFloat(req.query.x);
+    const y = parseFloat(req.query.y);
+
+    if (isNaN(x) || isNaN(y)) {
+        return res.status(400).send('Invalid numbers provided.');
+    }
+    res.send(`Result: ${x+y}`);
+});
+
+router.get ('/calculate', (req, res) => {
+    const a = Number(req.query.a);
+    const b = Number(req.query.b);
+    const operation = req.query.operation;
+
+    if (isNaN(a) || isNaN(b)) {
+        return res.status(400).send('Invalid numbers provided.');
+    }
+
+    let result;
+    switch(operation){
+        case '+':
+            result = a + b;
+            break;
+        case '-':
+            result = a - b;
+            break;
+        case '*':
+            result = a * b;
+            break;
+        case '/':
+            result = a / b;
+            break;   
+        case '**':
+            result = a ** b;
+            break;
+        default:
+            return res.status(400).send('Invalid operation. Use +, -, *,/ , **');     
+    }
+
+    res.send(`Result: ${result}`);
+})
+
+module.exports = router;
